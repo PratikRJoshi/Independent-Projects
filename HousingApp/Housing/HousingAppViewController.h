bed2412ee8c85b5891853b2eebf1c9d6a00416e1
PratikRJoshi <pratikjo@usc.edu>
2015-02-02 21:26:52 -0800

    Apparently removed the mach-o linker errors (had to put the deprecated header folder in the project..duh) Now need to remove the UIKit not found error. Trying to change the Framework Search Path field and checking...

--- HousingApp/Housing/HousingAppViewController.h
+++ HousingApp/Housing/HousingAppViewController.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <FacebookSDK/FacebookSDK.h>
+#import "FacebookSDK.h"
 #import <QuartzCore/QuartzCore.h>
 
 @interface HousingAppViewController : UIViewController<FBLoginViewDelegate>
